82175dd24a0ce800387cb62f6a00f9576b852622
Nacho Caiafa <nachokai@outlook.com>
2021-02-13 21:29:27 -0300

    add fibo and factorial functions

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,10 +1,22 @@
-import fp from 'lodash/fp';
+import fp from "lodash/fp";
 
-export const fibo = '???';
+export const fibo = (n, memo = {}) => {
+  if (n in memo) return memo[n];
+  if (n <= 2) return 1;
 
-export const factorial = '???';
+  if (n > 2) {
+    memo[n] = fibo(n - 1, memo) + fibo(n - 2, memo);
+    return memo[n];
+  }
+};
 
-export const multiplicacion = '???';
+export const factorial = (n, memo = {}) => {
+  if (n in memo) return memo[n];
+  if (n <= 0) return 1;
+  if (n >= 1) return n * factorial(n - 1, memo);
+};
+
+export const multiplicacion = "???";
 
 // Funciones de lodash/fp que les pueden ser útiles a partir de este punto:
 // Las vistas en la clase (particularmente fp.flow y fp.curry)
@@ -12,10 +24,10 @@ export const multiplicacion = '???';
 // fp.reverse (para dar vuelta un array)
 // fp.first (para obtener el primer valor de un array)
 
-export const atributo = '???';
+export const atributo = "???";
 
-export const multiplicarAtributo = '???';
+export const multiplicarAtributo = "???";
 
-export const ordenarPor = '???';
+export const ordenarPor = "???";
 
-export const mayorPersona = '???';
+export const mayorPersona = "???";
